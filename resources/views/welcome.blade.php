2bde5d18440982593d11c147a4b2a72d957a5a78
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2020-08-09 13:45:06 +0700

    add

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel act
                 </div>
 
                 <div class="links">
