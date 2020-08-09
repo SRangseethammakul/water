9c853f7bfec6008e9a58c29af795dfa6efe16797
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2020-08-09 14:32:48 +0700

    add front-end and make auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,72 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html>
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>AdminLTE 3 | Dashboard</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+</head>
 
-            .full-height {
-                height: 100vh;
-            }
+<body>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel act
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col align-self-center">
+                <img src="{{ asset('images/water.png') }}" width="10%" style=" display: block;margin-left: auto;margin-right: auto">
             </div>
         </div>
-    </body>
+        <div class="row mt-5 mb-5">
+            <input class="form-control form-control-lg" id="search" type="text" placeholder="ชื่อร้าน เบอร์โทร เจ้าของร้าน">
+        </div>
+        <div id="result"></div>
+    </div>
+
+
+
+<script src="{{ asset('js/app.js') }}"></script>
+<script>
+    var _xhr;
+    function startSearch(){
+        var search = $('#search').val();
+        _xhr = $.ajax({
+            url: '/search',
+            method: 'GET',
+            data: {
+                search : search
+            },
+            success: function (response) {
+                if (response.status == 1) {
+                    $.each(response.data, function(index,item) {
+                        $store_promotion = 0;
+                        console.log(item.store_tel);
+                        var html_q =
+                        `
+                        <div class="jumbotron">
+                            <h1 class="display-4">`+ item.store_name + `</h1>
+                            <p class="lead">ติดต่อคุณ `+ item.store_contact + `</p>
+                            <p class="lead">เบอร์ติดต่อ `+ item.store_tel + `</p>
+                            <a class="btn btn-primary btn-lg" href="detail/`+item.id+`" role="button">Learn more</a>
+                        </div>
+                         `
+                        $("#result").append(html_q);
+                    });
+                }   
+            }
+        });
+    }
+    $('#search').on('keyup', function() {
+        _xhr && _xhr.abort();
+        $('#result').html('');
+        startSearch();
+    });
+</script>
+</body>
+
 </html>
