e4f9c0ffafe1e1a1e415575f1fff3da892f1a8c5
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2020-08-09 15:04:04 +0700

    add promotion

---
+++ database/migrations/2020_08_09_080247_create_promotions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePromotionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('promotions', function (Blueprint $table) {
+            $table->id();
+            $table->string('promotion_name')->nullable();
+            $table->text('promotion_detail')->nullable();
+            $table->string('promotion_status')->nullable();
+            $table->string('promotion_price')->nullable();
+            $table->string('promotion_sum_price')->nullable();
+            $table->string('promotion_image')->nullable();
+            $table->foreignId('type_id')->references('id')->on('types')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('promotions');
+    }
+}
