dc7bca2adca6f9813314838e7a8a4290ba5c196a
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2020-08-09 15:00:53 +0700

    add type

---
+++ database/migrations/2020_08_09_075952_create_types_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('types', function (Blueprint $table) {
+            $table->id();
+            $table->string('type_name')->nullable();
+            $table->text('type_detail')->nullable();
+            $table->string('type_status')->nullable();
+            $table->string('type_price_sum')->nullable();
+            $table->string('type_image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('types');
+    }
+}
