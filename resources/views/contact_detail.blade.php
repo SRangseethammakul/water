9d83f7066004a28ac34b31ab1ff005cfa9660522
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2020-09-11 22:07:37 +0700

    d

--- resources/views/contact_detail.blade.php
+++ resources/views/contact_detail.blade.php
@@ -1,147 +1,167 @@
-<!DOCTYPE html>
-<html>
+@extends('layouts.frontend')
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Detail</title>
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
-</head>
-
-<body>
-    <div class="row mt-5" style="margin-right: 0px">
-        <div class="col align-self-center">
-            <img src="{{ asset('images/water.png') }}" width="10%" style=" display: block;margin-left: auto;margin-right: auto">
-        </div>
+@section('content')
+<div class="container">
+  <div class="row mt-5" style="margin-right: 0px">
+    <div class="col align-self-center">
+        <img src="{{ asset('images/water.png') }}" width="10%" style=" display: block;margin-left: auto;margin-right: auto">
     </div>
+</div>
 
-    <div class="container mt-5">
-        <h1 class="display-4">{{$store->store_name}}</h1>
+<div class="container mt-5">
+    <h1 class="display-4">{{$store->store_name}}</h1>
 
-        <div class="accordion" id="accordionExample">
-            <div class="card">
-              <div class="card-header" id="headingOne">
-                <h2 class="mb-0">
-                  <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                    รายละเอียดร้านค้า
-                  </button>
-                </h2>
-              </div>
-          
-              <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-                <div class="card-body">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-4 offset-md-2"><h2>ชื่อร้าน</h2></div>
-                            <div class="col-md-4 ml-auto"><h2>{{$store->store_name}}</h2></div>
-                        </div>
-                        <hr>
-                        <div class="row">
-                            <div class="col-md-4 offset-md-2"><h2>เบอร์ติดต่อ</h2></div>
-                            <div class="col-md-4 ml-auto">
-                                <a href="tel:{{$store->store_tel}}">
-                                <h2>{{$store->store_tel}}</h2></a>
-                            </div>
-                        </div>
-                        <hr>
-                        <div class="row">
-                            <div class="col-md-4 offset-md-2"><h2>สถานนะร้านค้า</h2></div>
-                            <div class="col-md-4 ml-auto"><h2>{{$store->store_status ? "ปกติ" : "ปิดกิจการ"}}</h2></div>
-                        </div>
-                        <hr>
-                        <div class="row">
-                            <div class="col-md-4 offset-md-2"><h2>ประเภทสินค้า</h2></div>
-                            <div class="col-md-4 ml-auto"><h2>{{$store->store_contact}}</h2></div>
-                        </div>
+    <div class="accordion" id="accordionExample">
+        <div class="card">
+          <div class="card-header" id="headingOne">
+            <h2 class="mb-0">
+              <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                รายละเอียดร้านค้า
+              </button>
+            </h2>
+          </div>
+      
+          <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+            <div class="card-body">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4 offset-md-2"><h2>ชื่อร้าน</h2></div>
+                        <div class="col-md-4 ml-auto"><h2>{{$store->store_name}}</h2></div>
                     </div>
-                </div>
-              </div>
-            </div>
-            <div class="card">
-              <div class="card-header" id="headingTwo">
-                <h2 class="mb-0">
-                  <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                    โปรโมชันร้านค้า
-                  </button>
-                </h2>
-              </div>
-              <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-                @foreach ($promotions as $item)
-                    <div class="card-body">
-                        <div class="container">
-                          <div class="row">
-                              <div class="col-md-4 offset-md-2"><h2>ชื่อโปรโมชัน</h2></div>
-                              <div class="col-md-4 ml-auto"><h2>{{$item->promotion_name}}</h2></div>
-                          </div>   
-                          <hr>
-                          <div class="row">
-                              <div class="col-md-4 offset-md-2"><h2>รายละเอียดโปรโมชัน</h2></div>
-                              <div class="col-md-4 ml-auto"><h2>{{$item->promotion_detail}}</h2></div>
-                          </div>
-                          <hr>
-                          <div class="row justify-content-center">
-                              <div class="col-md-4 offset-md-2"><h2>ราคา</h2></div>
-                              <div class="col-md-4 ml-auto"><h2>{{$item->promotion_price}}</h2></div>
-                          </div>
-                          <hr>
-                          <div class="row justify-content-center">
-                              <div class="col-md-4 offset-md-2"><h2>ประเภทสินค้า</h2></div>
-                              <div class="col-md-4 ml-auto"><h2>{{$item->product->product_name}}</h2></div>
-                          </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-md-4 offset-md-2"><h2>เบอร์ติดต่อ</h2></div>
+                        <div class="col-md-4 ml-auto">
+                            <a href="tel:{{$store->store_tel}}">
+                            <h2>{{$store->store_tel}}</h2></a>
                         </div>
                     </div>
                     <hr>
-                @endforeach    
-              </div>
-            </div>
-            <div class="card">
-              <div class="card-header" id="headingThree">
-                <h2 class="mb-0">
-                  <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                    ข้อมูลใบกำกับภาษี
-                  </button>
-                </h2>
-              </div>
-              <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-                <div class="card-body">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-4 offset-md-2"><h2>หมายเลขผู้เสียภาษี</h2></div>
-                            <div class="col-md-4 ml-auto"><h2>{{$store->store_tax_id}}</h2></div>
-                        </div>
-                        <hr>
-                        <div class="row justify-content-center">
-                            <div class="col-md-4 offset-md-2"><h2>ชื่อผู้เสียภาษี</h2></div>
-                            <div class="col-md-4 ml-auto">
-                                <h2>{{$store->store_tax_name}}</h2></a>
-                            </div>
-                        </div>
-                        <hr>
-                        <div class="row justify-content-center">
-                            <div class="col-md-4 offset-md-2"><h2>ที่อยู่ผู้เสียภาษี</h2></div>
-                            <div class="col-md-4 ml-auto"><h2>{{$store->store_tax_contact}}</h2></div>
-                        </div>
+                    <div class="row">
+                        <div class="col-md-4 offset-md-2"><h2>สถานนะร้านค้า</h2></div>
+                        <div class="col-md-4 ml-auto"><h2>{{$store->store_status ? "ปกติ" : "ปิดกิจการ"}}</h2></div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-md-4 offset-md-2"><h2>ประเภทสินค้า</h2></div>
+                        <div class="col-md-4 ml-auto"><h2>{{$store->store_contact}}</h2></div>
                     </div>
                 </div>
-              </div>
             </div>
+          </div>
         </div>
-        <a href="/">
-            <button type="button" class="btn btn-primary btn-lg btn-block">กลับไปค้นหาร้านค้า</button>
-        </a>
+        <div class="card">
+          <div class="card-header" id="headingTwo">
+            <h2 class="mb-0">
+              <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                โปรโมชันร้านค้า
+              </button>
+            </h2>
+          </div>
+          <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+            @foreach ($promotions as $item)
+                <div class="card-body">
+                    <div class="container">
+                      <div class="row">
+                          <div class="col-md-4 offset-md-2"><h2>ชื่อโปรโมชัน</h2></div>
+                          <div class="col-md-4 ml-auto"><h2>{{$item->promotion_name}}</h2></div>
+                      </div>   
+                      <hr>
+                      <div class="row">
+                          <div class="col-md-4 offset-md-2"><h2>รายละเอียดโปรโมชัน</h2></div>
+                          <div class="col-md-4 ml-auto"><h2>{{$item->promotion_detail}}</h2></div>
+                      </div>
+                      <hr>
+                      <div class="row justify-content-center">
+                          <div class="col-md-4 offset-md-2"><h2>ราคา</h2></div>
+                          <div class="col-md-4 ml-auto"><h2>{{$item->promotion_price}}</h2></div>
+                      </div>
+                      <hr>
+                      <div class="row justify-content-center">
+                          <div class="col-md-4 offset-md-2"><h2>ประเภทสินค้า</h2></div>
+                          <div class="col-md-4 ml-auto"><h2>{{$item->product->product_name}}</h2></div>
+                      </div>
+                    </div>
+                </div>
+                <hr>
+            @endforeach    
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-header" id="headingThree">
+            <h2 class="mb-0">
+              <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                ข้อมูลใบกำกับภาษี
+              </button>
+            </h2>
+          </div>
+          <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+            <div class="card-body">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4 offset-md-2"><h2>หมายเลขผู้เสียภาษี</h2></div>
+                        <div class="col-md-4 ml-auto"><h2>{{$store->store_tax_id}}</h2></div>
+                    </div>
+                    <hr>
+                    <div class="row justify-content-center">
+                        <div class="col-md-4 offset-md-2"><h2>ชื่อผู้เสียภาษี</h2></div>
+                        <div class="col-md-4 ml-auto">
+                            <h2>{{$store->store_tax_name}}</h2></a>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row justify-content-center">
+                        <div class="col-md-4 offset-md-2"><h2>ที่อยู่ผู้เสียภาษี</h2></div>
+                        <div class="col-md-4 ml-auto"><h2>{{$store->store_tax_contact}}</h2></div>
+                    </div>
+                </div>
+            </div>
+          </div>
+        </div>
     </div>
-
-
-
-
+    <a href="/">
+        <button type="button" class="btn btn-primary btn-lg btn-block">กลับไปค้นหาร้านค้า</button>
+    </a>
+</div>
+</div>
+@endsection
+@section('footerscript')
 <script src="{{ asset('js/app.js') }}"></script>
-
-</body>
-
-</html>
+<script>
+    var _xhr;
+    function startSearch(){
+        var search = $('#search').val();
+        _xhr = $.ajax({
+            url: '/search',
+            method: 'GET',
+            data: {
+                search : search
+            },
+            success: function (response) {
+                if (response.status == 1) {
+                    $.each(response.data, function(index,item) {
+                        var html_q =
+                        `
+                        <div class="jumbotron">
+                            <h1 class="display-4">`+ item.store_name + `</h1>
+                            <p class="lead">ติดต่อคุณ `+ item.store_contact + `</p>
+                            <p class="lead">เบอร์ติดต่อ `+ item.store_tel + `</p>
+                            <a class="btn btn-info btn-lg" href="detail/`+item.id+`" role="button">ดูข้อมูลเพิ่มเติม</a>
+                        </div>
+                         `
+                        $("#result").append(html_q);
+                    });
+                }   
+            }
+        });
+    }
+    $('#search').on('keyup', function() {
+        _xhr && _xhr.abort();
+        $('#result').html('');
+        var num = $('#search').val();
+        if(num.length > 0){
+            startSearch();
+        }
+    });
+</script>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,36 +1,19 @@
-<!DOCTYPE html>
-<html>
+@extends('layouts.frontend')
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>ค้นหาร้านค้า</title>
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
-</head>
-
-<body>
-
-    <div class="container">
-        <div class="row mt-5">
-            <div class="col align-self-center">
-                <img src="{{ asset('images/water.png') }}" width="10%" style=" display: block;margin-left: auto;margin-right: auto">
-            </div>
+@section('content')
+<div class="container">
+    <div class="row mt-5">
+        <div class="col align-self-center">
+            <img src="{{ asset('images/water.png') }}" width="10%" style=" display: block;margin-left: auto;margin-right: auto">
         </div>
-        <div class="row mt-5 mb-5">
-            <input class="form-control form-control-lg" id="search" type="text" placeholder="ชื่อร้าน เบอร์โทร เจ้าของร้าน">
-        </div>
-        <div id="result"></div>
     </div>
-
-
-
-<script src="{{ asset('js/app.js') }}"></script>
+    <div class="row mt-5 mb-5">
+        <input class="form-control form-control-lg" id="search" type="text" placeholder="ชื่อร้าน เบอร์โทร เจ้าของร้าน">
+    </div>
+    <div id="result"></div>
+</div>
+@endsection
+@section('footerscript')
 <script>
     var _xhr;
     function startSearch(){
@@ -63,12 +46,9 @@
         _xhr && _xhr.abort();
         $('#result').html('');
         var num = $('#search').val();
-        console.log(num.length > 0, num.length);
         if(num.length > 0){
             startSearch();
         }
     });
 </script>
-</body>
-
-</html>
+@endsection
