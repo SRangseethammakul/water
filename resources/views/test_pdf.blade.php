8da8a79c368c782318052a89cdc36244e654ab77
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2021-01-09 01:45:57 +0700

    test for th

--- resources/views/test_pdf.blade.php
+++ resources/views/test_pdf.blade.php
@@ -1,17 +1,10 @@
 <html>
-<header>
-<title>pdf</title>
-<meta http-equiv=”Content-Language” content=”th” />
-<meta http-equiv=”Content-Type” content=”text/html; charset=utf-8" />
-<link href=”https://fonts.googleapis.com/css2?family=Sarabun:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel=”stylesheet”>
-<style>
-body {
-font-family: ‘sarabun’, sans-serif;
-font-size: 20px;
-}
-</style>
-</header>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+</head>
 <body>
-<p>สวัสดี</p> ผม สร้างจาก domPDF
+<h1>ใบแจ้งหนี้สำหรับ </h1>
+ขอขอบคุณในการสั่งซื้อ
+
 </body>
 </html>
