d262e6b8137e49855869ae304028be5b7082c10c
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2021-01-09 01:48:10 +0700

    #2

--- resources/views/test_pdf.blade.php
+++ resources/views/test_pdf.blade.php
@@ -1,9 +1,50 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <style>
+        @font-face{
+         font-family:  'THSarabunNew';
+         font-style: normal;
+         font-weight: normal;
+         src: url("{{ asset('fonts/THSarabunNew.ttf') }}") format('truetype');
+        }
+        @font-face{
+         font-family:  'THSarabunNew';
+         font-style: normal;
+         font-weight: bold;
+         src: url("{{ asset('fonts/THSarabunNew Bold.ttf') }}") format('truetype');
+        }
+        @font-face{
+         font-family:  'THSarabunNew';
+         font-style: italic;
+         font-weight: normal;
+         src: url("{{ asset('fonts/THSarabunNew Italic.ttf') }}") format('truetype');
+        }
+        @font-face{
+         font-family:  'THSarabunNew';
+         font-style: italic;
+         font-weight: bold;
+         src: url("{{ asset('fonts/THSarabunNew BoldItalic.ttf') }}") format('truetype');
+        }
+        body{
+         font-family: "THSarabunNew";
+         font-size: 16px;
+        }
+        @page {
+              size: A4;
+              padding: 15px;
+            }
+        @media print {
+            html, body {
+            width: 210mm;
+            height: 297mm;
+            /*font-size : 16px;*/
+            }
+        }
+    </style>
 </head>
 <body>
-<h1>ใบแจ้งหนี้สำหรับ </h1>
+<h1>ใบแจ้งหนี้สำหรับ {{ $name }}</h1>
 ขอขอบคุณในการสั่งซื้อ
 
 </body>
