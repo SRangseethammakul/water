867ad1e473ce35eb8a50b4f43e2eff27c5d9db06
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2021-01-09 02:02:57 +0700

    dd

--- resources/views/test_pdf.blade.php
+++ resources/views/test_pdf.blade.php
@@ -2,44 +2,33 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <style>
-        @font-face{
-         font-family:  'THSarabunNew';
-         font-style: normal;
-         font-weight: normal;
-         src: url("{{ asset('fonts/THSarabunNew.ttf') }}") format('truetype');
+        @font-face {
+            font-family: 'THSarabunNew';
+            font-style: normal;
+            font-weight: normal;
+            src: url("{{ public_path('fonts/THSarabunNew.ttf') }}") format('truetype');
         }
-        @font-face{
-         font-family:  'THSarabunNew';
-         font-style: normal;
-         font-weight: bold;
-         src: url("{{ asset('fonts/THSarabunNew Bold.ttf') }}") format('truetype');
+        @font-face {
+            font-family: 'THSarabunNew';
+            font-style: normal;
+            font-weight: bold;
+            src: url("{{ public_path('fonts/THSarabunNew Bold.ttf') }}") format('truetype');
         }
-        @font-face{
-         font-family:  'THSarabunNew';
-         font-style: italic;
-         font-weight: normal;
-         src: url("{{ asset('fonts/THSarabunNew Italic.ttf') }}") format('truetype');
+        @font-face {
+            font-family: 'THSarabunNew';
+            font-style: italic;
+            font-weight: normal;
+            src: url("{{ public_path('fonts/THSarabunNew Italic.ttf') }}") format('truetype');
         }
-        @font-face{
-         font-family:  'THSarabunNew';
-         font-style: italic;
-         font-weight: bold;
-         src: url("{{ asset('fonts/THSarabunNew BoldItalic.ttf') }}") format('truetype');
+        @font-face {
+            font-family: 'THSarabunNew';
+            font-style: italic;
+            font-weight: bold;
+            src: url("{{ public_path('fonts/THSarabunNew BoldItalic.ttf') }}") format('truetype');
         }
-        body{
-         font-family: "THSarabunNew";
-         font-size: 16px;
-        }
-        @page {
-              size: A4;
-              padding: 15px;
-            }
-        @media print {
-            html, body {
-            width: 210mm;
-            height: 297mm;
-            /*font-size : 16px;*/
-            }
+
+        body {
+            font-family: "THSarabunNew";
         }
     </style>
 </head>
