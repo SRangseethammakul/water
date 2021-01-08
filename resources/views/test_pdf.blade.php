0aca3024501ac4e1072a5e0f06d8adb45ca26808
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2021-01-09 02:26:51 +0700

    as

--- resources/views/test_pdf.blade.php
+++ resources/views/test_pdf.blade.php
@@ -62,5 +62,12 @@
     สุทธิพงศ์ รังสีธรรมกุล
 </div> --}}
 
+<div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">สุทธิพงศ์ รังสีธรรมกุล</h1>
+      <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+    </div>
+</div>
+
 </body>
 </html>
