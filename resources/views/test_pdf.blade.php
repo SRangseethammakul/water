a72200440623d6562c30a1677b00b75c27b3003d
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2021-01-09 01:42:55 +0700

    test pdf

--- resources/views/test_pdf.blade.php
+++ resources/views/test_pdf.blade.php
@@ -1,43 +1,17 @@
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<html>
+<header>
+<title>pdf</title>
+<meta http-equiv=”Content-Language” content=”th” />
+<meta http-equiv=”Content-Type” content=”text/html; charset=utf-8" />
+<link href=”https://fonts.googleapis.com/css2?family=Sarabun:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel=”stylesheet”>
 <style>
-    @font-face{
-     font-family:  'THSarabunNew';
-     font-style: normal;
-     font-weight: normal;
-     src: url("{{ asset('fonts/THSarabunNew.ttf') }}") format('truetype');
-    }
-    @font-face{
-     font-family:  'THSarabunNew';
-     font-style: normal;
-     font-weight: bold;
-     src: url("{{ asset('fonts/THSarabunNew Bold.ttf') }}") format('truetype');
-    }
-    @font-face{
-     font-family:  'THSarabunNew';
-     font-style: italic;
-     font-weight: normal;
-     src: url("{{ asset('fonts/THSarabunNew Italic.ttf') }}") format('truetype');
-    }
-    @font-face{
-     font-family:  'THSarabunNew';
-     font-style: italic;
-     font-weight: bold;
-     src: url("{{ asset('fonts/THSarabunNew BoldItalic.ttf') }}") format('truetype');
-    }
-    body{
-     font-family: "THSarabunNew";
-     font-size: 16px;
-    }
-    @page {
-          size: A4;
-          padding: 15px;
-        }
-        @media print {
-          html, body {
-            width: 210mm;
-            height: 297mm;
-            /*font-size : 16px;*/
-          }
-        }
-    </style>
-    <div>สวัสดีวันจันทร์</div>
+body {
+font-family: ‘sarabun’, sans-serif;
+font-size: 20px;
+}
+</style>
+</header>
+<body>
+<p>สวัสดี</p> ผม สร้างจาก domPDF
+</body>
+</html>
