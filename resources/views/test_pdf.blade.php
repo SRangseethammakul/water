29c23a900ca22c26ecdcd1958567e5411cb07fe4
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2021-01-09 02:11:05 +0700

    a

--- resources/views/test_pdf.blade.php
+++ resources/views/test_pdf.blade.php
@@ -45,7 +45,7 @@
 </head>
 <body>
     <td align="center">
-        <img src="{{ public_path('images/water.png') }}g" alt="Logo" width="64" class="logo"/>
+        <img src="{{ public_path('images/water.png') }}" alt="Logo" width="64" class="logo"/>
     </td>
 <h1>ใบแจ้งหนี้สำหรับ</h1>
 ขอขอบคุณในการสั่งซื้อ
