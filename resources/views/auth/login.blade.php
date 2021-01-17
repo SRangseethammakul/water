50fd9e996c0ef7deec2cc4fa9dbddf4df04cc40b
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2021-01-17 14:35:30 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -71,12 +71,14 @@
                               <div class="col align-self-center">
                                 <a href="{{ url('auth/github') }}" style="text-decoration: none;">
                                     <button type="button" class="btn btn-outline-secondary btn-block ml-2 mt-2" style="padding: 12px 16px">
-                                        <div class="row justify-content-center">
-                                            <div class="col-4">
-                                                <img src="{{ asset('images/github.png') }}" height="50" alt="github">
-                                            </div>
-                                            <div class="col-4">
-                                                <p class="text-center mt-3">ดำเนินการต่อด้วย GitHub</p>
+                                        <div class="container">
+                                            <div class="row justify-content-center">
+                                                <div class="col-4">
+                                                    <img src="{{ asset('images/github.png') }}" height="50" alt="github">
+                                                </div>
+                                                <div class="col-4">
+                                                    <p class="text-center">ดำเนินการต่อด้วย GitHub</p>
+                                                </div>
                                             </div>
                                         </div>
                                     </button>
@@ -92,24 +94,28 @@
                                 </a> --}}
                                 <a href="{{ url('auth/facebook') }}" style="text-decoration: none;">
                                     <button type="button" class="btn btn-outline-info btn-block ml-2 mt-2" style="padding: 12px 16px">
-                                        <div class="row justify-content-center">
-                                            <div class="col-4">
-                                                <img src="{{ asset('images/facebook.png') }}" height="50" alt="facebook">
-                                            </div>
-                                            <div class="col-4">
-                                                <p class="text-center mt-3">ดำเนินการต่อด้วย Facebook</p>
+                                        <div class="container">
+                                            <div class="row justify-content-center">
+                                                <div class="col-4">
+                                                    <img src="{{ asset('images/facebook.png') }}" height="50" alt="facebook">
+                                                </div>
+                                                <div class="col-4">
+                                                    <p class="text-center">ดำเนินการต่อด้วย Facebook</p>
+                                                </div>
                                             </div>
                                         </div>
                                     </button>
                                 </a>
                                 <a href="{{ url('auth/line') }}" style="text-decoration: none;">
                                     <button type="button" class="btn btn-outline-success btn-block ml-2 mt-2" style="padding: 12px 16px">
-                                        <div class="row justify-content-center">
-                                            <div class="col-4">
-                                                <img src="{{ asset('images/line.png') }}" height="50" alt="line">
-                                            </div>
-                                            <div class="col-4">
-                                                <p class="text-center mt-3">ดำเนินการต่อด้วย Line</p>
+                                        <div class="container">
+                                            <div class="row justify-content-center">
+                                                <div class="col-4">
+                                                    <img src="{{ asset('images/line.png') }}" height="50" alt="line">
+                                                </div>
+                                                <div class="col-4">
+                                                    <p class="text-center">ดำเนินการต่อด้วย Line</p>
+                                                </div>
                                             </div>
                                         </div>
                                     </button>
