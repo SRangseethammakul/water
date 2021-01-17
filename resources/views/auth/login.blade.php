da5e90b0bf0cdb7fcae0b4dbb22603469da9c862
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2021-01-17 14:16:18 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -71,19 +71,27 @@
                               <div class="col align-self-center">
                                 <a href="{{ url('auth/github') }}">
                                     <button type="button" class="btn btn-outline-secondary btn-block ml-2" style="padding: 12px 16px"><i
-                                            class="fa fa-github" style="margin:5px 15px 5px 5px" aria-hidden="true"></i>Login
-                                        with GitHub</button>
+                                            class="fa fa-github" style="margin:5px 15px 5px 5px" aria-hidden="true"></i> ดำเนินการต่อด้วย GitHub</button>
                                 </a>
                                 <a href="{{ url('auth/facebook') }}">
                                     <button type="button" class="btn btn-outline-info btn-block ml-2 mt-2" style="padding: 12px 16px"><i
-                                            class="fa fa-facebook ml-2" style="margin:5px 15px 5px 5px" aria-hidden="true"></i>Login
-                                        with Facebook</button>
+                                            class="fa fa-facebook ml-2" style="margin:5px 15px 5px 5px" aria-hidden="true"></i>ดำเนินการต่อด้วย Facebook</button>
                                 </a>
                                 <a href="{{ url('auth/line') }}">
-                                    <button type="button" class="btn btn-outline-success btn-block ml-2 mt-2" style="padding: 12px 16px"><i
-                                            class="fa fa-facebook ml-2" style="margin:5px 15px 5px 5px" aria-hidden="true"></i>Login
-                                        with Line</button>
+                                    <button type="button" class="btn btn-outline-success btn-block ml-2 mt-2" style="padding: 12px 16px">
+                                        <div class="row justify-content-around">
+                                            <div class="col-4">
+                                                <img src="{{ asset('images/line.png') }}" height="50" alt="line">
+                                            </div>
+                                            <div class="col-4">
+                                                <p class="text-center">Center aligned text on all viewport sizes.</p>
+                                            </div>
+                                        </div>
+                                    </button>
                                 </a>
+                                
+
+                                <h1 class="text-hide" style="background-image: url('{{ asset('images/line.png') }}');">Bootstrap</h1>
                                 {{-- <a href="{{ url('auth/google') }}">
                                     <button type="button" class="btn btn-outline-success btn-block ml-2 mt-2" style="padding: 12px 16px"><i
                                             class="fa fa-google ml-2" style="margin:5px 15px 5px 5px" aria-hidden="true"></i>Login
