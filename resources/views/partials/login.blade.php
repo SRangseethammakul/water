9140bb97b0693dfcc94d8b956a63f902a3a6596a
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2021-01-17 14:47:14 +0700

    modal login

--- resources/views/partials/login.blade.php
+++ resources/views/partials/login.blade.php
@@ -72,20 +72,52 @@
                 <div class="container">
                     <div class="row">
                       <div class="col align-self-center">
-                        <a href="{{ url('auth/github') }}">
-                            <button type="button" class="btn btn-outline-secondary btn-block ml-2" style="padding: 12px 16px"><i
-                                    class="fa fa-github" style="margin:5px 15px 5px 5px" aria-hidden="true"></i>Login
-                                with GitHub</button>
+                        <a href="{{ url('auth/facebook') }}" style="text-decoration: none;">
+                            <button type="button" class="btn btn-outline-info btn-block ml-2 mt-2" style="padding: 12px 16px">
+                                <div class="container">
+                                    <div class="row justify-content-center">
+                                        <div class="col-4">
+                                            <img src="{{ asset('images/facebook.png') }}" height="50" alt="facebook">
+                                        </div>
+                                        <div class="col-8">
+                                            <p class="text-center">ดำเนินการต่อด้วย Facebook</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </button>
                         </a>
-                        <a href="{{ url('auth/facebook') }}">
+                        {{-- <a href="{{ url('auth/facebook') }}">
                             <button type="button" class="btn btn-outline-info btn-block ml-2 mt-2" style="padding: 12px 16px"><i
                                     class="fa fa-facebook ml-2" style="margin:5px 15px 5px 5px" aria-hidden="true"></i>Login
                                 with Facebook</button>
+                        </a> --}}
+                        <a href="{{ url('auth/facebook') }}" style="text-decoration: none;">
+                            <button type="button" class="btn btn-outline-info btn-block ml-2 mt-2" style="padding: 12px 16px">
+                                <div class="container">
+                                    <div class="row justify-content-center">
+                                        <div class="col-4">
+                                            <img src="{{ asset('images/facebook.png') }}" height="50" alt="facebook">
+                                        </div>
+                                        <div class="col-8">
+                                            <p class="text-center">ดำเนินการต่อด้วย Facebook</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </button>
                         </a>
-                        <a href="{{ url('auth/line') }}">
-                            <button type="button" class="btn btn-outline-success btn-block ml-2 mt-2" style="padding: 12px 16px"><i
-                                    class="fa fa-facebook ml-2" style="margin:5px 15px 5px 5px" aria-hidden="true"></i>Login
-                                with Line</button>
+                        <a href="{{ url('auth/line') }}" style="text-decoration: none;">
+                            <button type="button" class="btn btn-outline-success btn-block ml-2 mt-2" style="padding: 12px 16px">
+                                <div class="container">
+                                    <div class="row justify-content-center">
+                                        <div class="col-4">
+                                            <img src="{{ asset('images/line.png') }}" height="50" alt="line">
+                                        </div>
+                                        <div class="col-8">
+                                            <p class="text-center">ดำเนินการต่อด้วย Line</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </button>
                         </a>
                         {{-- <a href="{{ url('auth/google') }}">
                             <button type="button" class="btn btn-outline-success btn-block ml-2 mt-2" style="padding: 12px 16px"><i
