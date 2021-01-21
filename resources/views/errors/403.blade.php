683756a1d2830705ee322702d8416fd631e3375e
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2021-01-21 22:15:00 +0700

    Update 403 page

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -7,7 +7,29 @@
         <p class="lead">ขออภัย บัญชีของท่านถูกระงับการใช้งานชั่วคราว</p>
         <hr class="my-4">
         <a class="btn btn-success btn-lg" href="{{ route('welcome') }}" role="button">กลับสู่หน้าแรก</a>
+        <button type="button" class="btn btn-primary" data-toggle="modal403" data-target="#exampleModal403">
+            ติดต่อเรา
+        </button>
     </div>
 </span>
+<!-- Modal -->
+<div class="modal fade" id="exampleModal403" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">ติดต่อเรา</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+            <h1 class="display-4">0817362947</h1>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+</div>
 @endsection
 
