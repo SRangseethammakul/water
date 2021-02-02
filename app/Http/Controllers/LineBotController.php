3df364b302cfd27b2a22b3e0bcd25b72f21a5815
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2021-02-02 21:41:54 +0700

    u

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -28,7 +28,53 @@ class LineBotController extends Controller
             $arrPostData = array();
             $arrPostData['messages'][0]['type'] = "flex";
             $arrPostData['messages'][0]['altText'] = "From broadcast";
-            $arrPostData['messages'][0]['text'] = "dwefefe";
+            $arrPostData['messages'][0]['contents'] = '                "type": "bubble",
+            "hero": {
+                "type": "image",
+                "url": "https://water-systems.sgp1.digitaloceanspaces.com/products/5f43ccf4abc97.png",
+                "size": "full",
+                "aspectRatio": "12:13",
+                "aspectMode": "cover",
+                "action": {
+                    "type": "uri",
+                    "uri": "http://suttipongact.info"
+                }
+            },
+            "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                    {
+                        "type": "text",
+                        "text": "RECEIPT",
+                        "weight": "bold",
+                        "color": "#1DB446",
+                        "size": "sm"
+                    },
+                    {
+                        "type": "text",
+                        "text": "Brown Store",
+                        "weight": "bold",
+                        "size": "xxl",
+                        "margin": "md"
+                    },
+                    {
+                        "type": "text",
+                        "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
+                        "size": "xs",
+                        "color": "#aaaaaa",
+                        "wrap": true
+                    },
+                    {
+                        "type": "separator",
+                        "margin": "xxl"
+                    },
+                    {
+                        "type": "text",
+                        "text": "World!"
+                    }
+                ]
+            }';
              
              
             $ch = curl_init();
